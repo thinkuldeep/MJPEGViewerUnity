eae2dfda61e70a724299ddd1c1f87762d90bf36d
Kuldeep Singh <kuldeeps@thoughtworks.com>
2019-11-18 16:55:00 +0530

    add MPEG Viewer

---
+++ Assets/Scripts/MjpegViewer.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class MjpegViewer : MonoBehaviour
+{
+   
+    [SerializeField] private InputField inputField;
+    
+    const int ChunkSize = 4;
+    const int InitWidth = 2;
+    const int InitHeight = 2;
+    private Texture2D _tex;
+    private bool _updateFrame = false;
+    private MjpegStreamProcessor _mjpegStream;
+    private float _deltaTime = 0.0f;
+    private float _mjpegDeltaTime = 0.0f;
+    private Renderer _renderer;
+    private string streamAddress;
+
+    public void Start()
+    {
+        if (_mjpegStream != null)
+        {
+            _updateFrame = false;
+            _deltaTime = 0f;
+            _mjpegStream.StopStream();
+        }
+        _mjpegStream = new MjpegStreamProcessor(ChunkSize * 1024);
+        _mjpegStream.FrameReady += OnMjpegStreamFrameReady;
+        _mjpegStream.Error += OnMjpegStreamError;
+        streamAddress = inputField.text;
+        Uri mjpegAddress = new Uri(streamAddress);
+        _mjpegStream.ParseStream(mjpegAddress);
+        _tex = new Texture2D(InitWidth, InitHeight, TextureFormat.ARGB32, false);
+        _renderer = GetComponent<Renderer>();
+        _updateFrame = true;
+    }
+    private void OnMjpegStreamFrameReady(object sender, FrameReadyEventArgs e)
+    {
+        _updateFrame = true;
+    }
+    void OnMjpegStreamError(object sender, ErrorEventArgs e)
+    {
+        Debug.Log("Error received while reading the MJPEG.");
+    }
+    
+    void Update()
+    {
+        _deltaTime += Time.deltaTime;
+
+        if (_updateFrame)
+        {
+            _tex.LoadImage(_mjpegStream.CurrentFrame);
+            // tex.Apply();
+            // Assign texture to renderer's material.
+            _renderer.material.mainTexture = _tex;
+            _updateFrame = false;
+
+            _mjpegDeltaTime += (_deltaTime - _mjpegDeltaTime) * 0.2f;
+
+            _deltaTime = 0.0f;
+        }
+    }
+    void OnDestroy()
+    {
+        _mjpegStream.StopStream();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# MJPEGViewer
+This is a Display video from an Mjpeg Stream.
+
+To use:
+
+1. Open  "Assets/Scenes/MJPEGViewer.unity" in Unity3D.
+
+2. Add path for the MJEPG stream. Ref to : https://github.com/thinkuldeep/MJPEGServerUnity for building MJEPG Server. 
+
+3. Play the scene and provide the URL and click on Load. it should show the stream in the view port.
+
+4. You may also try installing the APK on andorid and cast game play on the mobile screen.
+
+Note:- the source code is derived from https://github.com/DanielArnett/SampleUnityMjpegViewer
+
+Enjoy!
